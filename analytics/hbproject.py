b888592976ddc3775c700f2451dc6a0ebf1bb124
tigerneil <xhzhu.nju@gmail.com>
2014-12-15 10:43:26 +0800

    updated.

--- analytics/hbproject.py
+++ analytics/hbproject.py
@@ -11,10 +11,12 @@ from pprint import pprint
 # for every country get its posts count 
 
 
-def get_country(data):
+def get_country():
 	country = {}
 	# decoded_data = json.loads(data)
-	for record in data:
+	for line in open('../data/final.jl'):
+		record = json.loads(line)
+		print record
 		country_name = record['country_name']
 		country.setdefault(country_name, 0)
 		for i in record['posts']:
@@ -24,7 +26,7 @@ def get_country(data):
 			score = score_of_count(view_count, comment_count, like_count)
 			country[country_name] += score
 			country[country_name] += score_of_text(i['text'])
-			print country[country_name]
+			# print country_name, country[country_name]
 	return country
 
 def score_of_count(view_count, comment_count, like_count):
@@ -40,8 +42,15 @@ def score_of_text(text):
 	
 
 if __name__ == '__main__':
-	data = [{'country_name': '中国','posts_count': 3.0, 'posts':[{'view_count':20000,'comment_count':100,'like_count':100,'publish_time':20131208,'text':'是健康的减肥卡拉斯京地方'}]},
-	{'country_name': '日本','posts_count': 3.0, 'posts':[{'view_count':30000,'comment_count':180,'like_count':1000,'publish_time':20131208,'text':'粉色的肌肤立刻睡觉地方'}]}]
-	result = get_country(data)
-	for i in result.keys():
-		print i + '\'s score: ' + str(result[i])
+	# data = [{'country_name': '中国','posts_count': 3.0, 'posts':[{'view_count':20000,'comment_count':100,'like_count':100,'publish_time':20131208,'text':'是健康的减肥卡拉斯京地方'}]},
+	# {'country_name': '日本','posts_count': 3.0, 'posts':[{'view_count':30000,'comment_count':180,'like_count':1000,'publish_time':20131208,'text':'粉色的肌肤立刻睡觉地方'}]}]
+	# # for line in open('../data/final.jl'):
+	# 	data=json.loads(line)
+	result = get_country()
+
+	f=open("result.txt",'wb')
+	f.write(json.dumps(result))
+	f.close()
+	# for i in result.keys():
+	# 	f.write(i + '\'s score: ' + str(result[i]))
+
